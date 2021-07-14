41ac2cdfa5cc63e94e4a425ca5ab79dc2d91be39
Innikolov <82927613+Innikolov@users.noreply.github.com>
2021-07-14 09:21:30 +0300

    2

--- Exersice_5.4/Exersice_5.4/main.c
+++ Exersice_5.4/Exersice_5.4/main.c
@@ -4,12 +4,12 @@
 
 // Work in progress! 
 
-int strend(char* str1, char* str2);
+int strend(char* s, char* t);
 
 int main() {
 
 	char* str1 = "hello there, how are you?";
-	char* str2 = " how are you oskj?";
+	char* str2 = " how are you?";
 
 	int result = strend(str1, str2);
 
@@ -17,19 +17,11 @@ int main() {
 }
 
 
-int strend(char* str1, char* str2) {
+	int strend(char* s, char* t)
+	{
+		if (strlen(s) < strlen(t)) return 0;
 
-	int s1;
-	int s2;
-	int diff = strlen(str1) - strlen(str2);
-
-	for (s1 = strlen(str1); s1 > diff && s1 - diff >= 0; s1--){
-		s2 = s1 - diff;
-
-		if (strcmp(str1[s1], str2[s2]) == 0){
-			return 1;
-		}
-		else return 0;
+		//s is an address, pointing to the beginning of string *s. Adding integers to s resolves 
+		// in changing the starting point of the string.
+		return !strcmp(s + strlen(s) - strlen(t), t);
 	}
-
-} 
